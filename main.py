6314c1a41692456506dea6a28b99407e82e8eea1
Aimer514 <531972741@qq.com>
2023-07-29 15:26:57 +0200

    benign model train

--- main.py
+++ main.py
@@ -1,16 +1,71 @@
-# This is a sample Python script.
+import torch
+from model import *
+from torch.utils.data import Dataset
+from torchvision import datasets, transforms
+import torch.nn as nn
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+dataset_path = "./data"
+device = "cpu"     # cuda:0
+epochs = 200
+dataset = "fmnist"
+batch_size = 128
+
+criterion = nn.CrossEntropyLoss()
+
+###### test model ###########
+
+def test_model(model, test_loader):
+    total_test_number = 0
+    correctly_labeled_samples = 0
+    model.eval()
+    for batch_idx, (data, label) in enumerate(test_loader):
+        data = data.to(device=device)
+        label = label.to(device=device)
+        output = model(data)
+        total_test_number += len(output)
+        _, pred_labels = torch.max(output, 1)
+        pred_labels = pred_labels.view(-1)
+
+        correctly_labeled_samples += torch.sum(torch.eq(pred_labels, label)).item()
+    model.train()
+    acc = correctly_labeled_samples / total_test_number
+    print('benign accuracy  = {}'.format(acc))
+    return acc
+
+######  model        #######
+model = ClassicCNN().to(device)
+
+####data loader        #####
+transforms_list = []
+transforms_list.append(transforms.ToTensor())
+mnist_transform = transforms.Compose(transforms_list)
+train_dataset = datasets.FashionMNIST(root = dataset_path, train=True, download=True, transform=mnist_transform)
+test_dataset = datasets.FashionMNIST(root = dataset_path, train=False, download=True, transform=mnist_transform)
+
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size = 128, shuffle = True)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size = 128, shuffle = True)
+
+#### train benign model ######
+
+optimizer = torch.optim.SGD(model.parameters(), lr=0.1, )  #lr=0.01, momentum=0.9, weight_decay=5e-4
+
+for epoch in range(epochs):
+    model.train()
+    print('current epoch  = {}'.format(epoch))
+    for batch_idx, (data, label) in enumerate(train_loader):
+        data = data.to(device)
+        label = label.to(device)
+        output = model(data)
+        optimizer.zero_grad()
+        loss = criterion(output, label.view(-1, ))
+        loss.backward()
+        optimizer.step()
+
+    print('loss  = {}'.format(loss))
+    test_model(model, test_loader)
+
+print('Benign model finish!')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
